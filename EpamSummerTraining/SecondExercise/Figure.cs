f719781efb4401abefefd42ad83b4a4bbfd55ef6
Kazarevich Mikhail <mihailkazarevich@mail.ru>
2020-07-11 23:34:48 +0300

    Начал второе задание. Добавио абстрактный класс фигур и вспомогательный класс для точек.

---
+++ EpamSummerTraining/SecondExercise/Figure.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstTask.SecondExercise
+{
+    public abstract class Figure
+    {
+        private protected Lazy<Point[]> _points = default;
+        private protected Lazy<double[]> _sideSizes = default;
+
+        public Point[] Points 
+        {
+            get
+            {
+                return _points.Value;
+            }
+        }
+
+        public double[] SideSizes
+        {
+            get
+            {
+                return _sideSizes.Value;
+            }
+        }
+
+        public Figure(params Point[] points)
+        {
+            _points = new Lazy<Point[]>(points);
+        }
+
+        public Figure(params double[] sideSizes)
+        {
+            _sideSizes = new Lazy<double[]>(sideSizes);
+        }
+
+        public abstract double GetAreaOfFigure();
+
+        public abstract double GetPerimeterOfFigure();
+
+    }
+}
---
+++ EpamSummerTraining/SecondExercise/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstTask.SecondExercise
+{
+    public class Point
+    {
+        /// <summary>
+        /// Значение точки на оси OY
+        /// </summary>
+        public double OrdinateAxisValue { get; set; }
+        /// <summary>
+        /// Значение точки на оси OX
+        /// </summary>
+        public double AbscissaAxisValue { get; set; }
+
+        public Point(double xPoint, double yPoint)
+        {
+            OrdinateAxisValue = yPoint;
+            AbscissaAxisValue = xPoint;
+        }
+    }
+}
