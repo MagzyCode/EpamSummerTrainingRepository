46636440392cb4bbcd3b3af7e13c0333e6a20fac
Kazarevich Mikhail <mihailkazarevich@mail.ru>
2020-07-12 01:19:05 +0300

    Добавлен класс треугольник

--- EpamSummerTraining/SecondExercise/Figure.cs
+++ EpamSummerTraining/SecondExercise/Figure.cs
@@ -6,8 +6,8 @@ namespace FirstTask.SecondExercise
 {
     public abstract class Figure
     {
-        private protected Lazy<Point[]> _points = default;
-        private protected Lazy<double[]> _sideSizes = default;
+        private protected Lazy<Point[]> _points = new Lazy<Point[]>();
+        private protected Lazy<double[]> _sideSizes = new Lazy<double[]>();
 
         public Point[] Points 
         {
@@ -25,12 +25,17 @@ namespace FirstTask.SecondExercise
             }
         }
 
-        public Figure(params Point[] points)
+        public Figure() 
+        { }
+
+        public Figure (Point[] points)
         {
             _points = new Lazy<Point[]>(points);
+            // var sideSizes = func?.Invoke();
+            // _sideSizes = new Lazy<double[]>(sideSizes);
         }
 
-        public Figure(params double[] sideSizes)
+        public Figure (double[] sideSizes)
         {
             _sideSizes = new Lazy<double[]>(sideSizes);
         }
@@ -38,6 +43,22 @@ namespace FirstTask.SecondExercise
         public abstract double GetAreaOfFigure();
 
         public abstract double GetPerimeterOfFigure();
+
+        private protected virtual double[] GetSideSizes()
+        {
+            var listOfSides = new List<double>();
+            for (int counter = 1; counter < Points.Length - 1; counter++)
+            {
+                var differenceAxisOrdinate = Math.Pow(Points[counter].OrdinateAxisValue -
+                    Points[counter + 1].OrdinateAxisValue, 2);
+                var differenceAxisAbscissa = Math.Pow(Points[counter].AbscissaAxisValue -
+                    Points[counter + 1].AbscissaAxisValue, 2);
+                var side = Math.Pow(differenceAxisAbscissa + differenceAxisOrdinate, 0.5);
+                listOfSides.Add(side);
+            }
+            var arrayOfSides = listOfSides.ToArray();
+            return arrayOfSides;
+        }
 
     }
 }
---
+++ EpamSummerTraining/SecondExercise/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstTask.SecondExercise
+{
+    class Triangle : Figure
+    {
+        
+        public Triangle(double[] sideSizes) : base(sideSizes)
+        { }
+
+        public Triangle(Point[] points) : base(points) 
+        { }
+
+        public override double GetAreaOfFigure()
+        {
+            double halfOfPerimeter = GetPerimeterOfFigure() / 2;
+            var tempResult = halfOfPerimeter * (halfOfPerimeter - SideSizes[0]) *
+                    (halfOfPerimeter - SideSizes[1]) * (halfOfPerimeter - SideSizes[2]);
+            var area = Math.Pow(tempResult, 0.5);
+            return area;
+        }
+
+        public override double GetPerimeterOfFigure()
+        {
+            //double perimeter = 0;
+            //if (SideSizes.Length != 0)
+            //{
+            //    perimeter = SideSizes.Sum();
+            //}
+            //else if (Points.Length != 0)
+            //{
+            //    double[] arrayOfSides = GetSideSizes();
+            //    perimeter = arrayOfSides.Sum(); 
+            //}
+            double perimeter = SideSizes.Sum();
+            return perimeter;
+        }
+    }
+}
